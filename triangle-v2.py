acb1f9994d514f4d3b42b8dfac9caa7750758ee3
LuvIsBadToTheBone <53568692+LuvIsBadToTheBone@users.noreply.github.com>
2022-11-04 03:52:59 +0100

    Update triangle-v2.py

--- triangle-v2.py
+++ triangle-v2.py
@@ -2,13 +2,9 @@ from PIL import Image, ImageDraw
 
 eingabe = int(input("Give depth of Sierpisky Triangle: "))
 
-def triangle(ausgabe):
-	image = Image.new("RGB", (eingabe, eingabe), "brown")
-	draw = ImageDraw.Draw(image)
-	for i in range(eingabe):
-		for j in range(eingabe):
-			draw.point((j|i,i), fill = "white")
-	image.save(ausgabe)
-
-if __name__ == "__main__":
-	triangle("triangle.png")
+image = Image.new("RGB", (eingabe, eingabe), "brown")
+draw = ImageDraw.Draw(image)
+for i in range(eingabe):
+	for j in range(eingabe):
+		draw.point((j|i,i), fill = "white")
+image.save("triangle.png")
